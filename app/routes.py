1cc6a18eda83d8d2a7d37aa20fd27c16624af238
bkmd11 <bkendall11@gmail.com>
2021-01-27 04:18:00 -0500

    made a rough table

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
 from flask import render_template, url_for, request
 from app import pugh_app
 
+
 @pugh_app.route('/', methods=['GET', 'POST'])
 @pugh_app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', title='Home Page')
+    listings = [
+        {'listing': 'A', 'rating': ['+', '=', '-']}
+        , {'listing': 'B', 'rating': ['-', '+', '=']}
+    ]
+
+    return render_template('index.html', title='Home Page', username='Brian', listings=listings)
