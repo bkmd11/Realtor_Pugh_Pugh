8ac7ef34287a925bb3cc54634be1a54c8cf21c8f
bkmd11 <bkendall11@gmail.com>
2021-01-29 06:25:52 -0500

    huzzah

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,23 @@
+import json
 from flask import render_template, url_for, request
 
 from app import pugh_app, db
-from app.models import User
+from app.models import User, Listing
 
 
 @pugh_app.route('/', methods=['GET', 'POST'])
 @pugh_app.route('/index', methods=['GET', 'POST'])
 def index():
     user = User.query.get(1)
+    listing = Listing.query.get(1)
+    rating = json.loads(listing.rating)
+    total = sum(rating)
+    rating.append(total)
     listings = [
-        {'listing': 'A', 'rating': ['+', '=', '+', '++']}
-        , {'listing': 'B', 'rating': ['-', '+', '=', '=']}
+        {'listing': listing.listing_name, 'rating': rating}
     ]
 
-    col_headers = ['Listings', 'Rural', 'Cheap', 'Spacious', 'Total']
+    col_headers = json.loads(user.ctq)
 
     return render_template('index.html',
                            title='Home Page',
