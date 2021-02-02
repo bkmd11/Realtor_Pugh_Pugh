10415152125f31e291207f88ff0586799b106ace
bkmd11 <bkendall11@gmail.com>
2021-02-02 10:24:16 -0500

    fixed database

--- app/models.py
+++ app/models.py
@@ -1,10 +1,19 @@
-from app import db
+from app import db, login
+
+from flask_login import UserMixin
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.dialects.sqlite import JSON
 
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
     id = Column(Integer, primary_key=True)
     username = Column(String(64), index=True, unique=True)
     email = Column(String(120), index=True, unique=True)
@@ -15,6 +24,17 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def user_listings(self):
+        listings = Listing.query.join(
+            User, (User.id == Listing.user_id)
+        )
 
 
 class Listing(db.Model):
--- app/routes.py
+++ app/routes.py
@@ -1,33 +1,55 @@
 import json
-from flask import render_template, url_for, request, redirect
+from flask import render_template, url_for, request, redirect, flash
+from flask_login import current_user, login_user, logout_user, login_required
+
+from werkzeug.urls import url_parse
 
 from app import pugh_app, db
 from app.models import User, Listing
-from app.forms import AddForm
+from app.forms import AddForm, LoginForm
 
 
 @pugh_app.route('/', methods=['GET', 'POST'])
 @pugh_app.route('/index', methods=['GET', 'POST'])
+@login_required
 def index():
-    user = User.query.get(1)
-    listing = Listing.query.get(1)
-    rating = json.loads(listing.rating)
-    total = sum(rating)
-    rating.append(total)
-    listings = [
-        {'listing': listing.listing_name, 'rating': rating}
-    ]
-
-    col_headers = json.loads(user.ctq)
+    listings = current_user.user_listings()
+   # rating = json.loads(listing.rating)
+   # total = sum(rating)
+   # rating.append(total)
+   #  listings = [
+   #      {'listing': listing.listing_name, 'rating': rating}
+   #  ]
+    print(listings)
+    col_headers = json.loads(current_user.ctq)
 
     return render_template('index.html',
                            title='Home Page',
-                           username=user.username,
+                           username=current_user.username,
                            listings=listings,
                            col_headers=col_headers)
 
 
+@pugh_app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+
 @pugh_app.route('/add_listing', methods=['GET', 'POST'])
+@login_required
 def add_listing():
     u = User.query.get(1)
     ctq = json.loads(u.ctq)
