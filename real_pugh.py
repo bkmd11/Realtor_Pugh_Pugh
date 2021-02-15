c1a72bd77e746b150098c5025bfcc9d8b63a695a
bkmd11 <bkendall11@gmail.com>
2021-02-14 20:35:15 -0500

    added sys to default shell

--- real_pugh.py
+++ real_pugh.py
@@ -1,7 +1,9 @@
+import sys
 from app import pugh_app, db
 
 from app.models import User, Listing
 
+
 @pugh_app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'lr': Listing}
+    return {'db': db, 'User': User, 'lr': Listing, 'sys': sys}
