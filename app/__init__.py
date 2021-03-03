f921fa3b35d33460a19688134668d04d8cd13dc8
bkmd11 <bkendall11@gmail.com>
2021-03-03 16:29:15 -0500

    error pages

--- app/__init__.py
+++ app/__init__.py
@@ -21,6 +21,9 @@ pugh_app.register_blueprint(auth_bp, url_prefix='/auth')
 from app.new_user import bp as new_user_bp
 pugh_app.register_blueprint(new_user_bp, url_prefix='/new_user')
 
+from app.errors import bp as error_bp
+pugh_app.register_blueprint(error_bp)
+
 from app.main import bp as main_bp
 pugh_app.register_blueprint(main_bp, url_prefix='/main')
 
